3a8b8b522052d5454e2652a756c60a2e50096f45
johny <45255501+Yahya-Elrahim@users.noreply.github.com>
2020-11-05 22:47:59 +0300

    Create main.java

---
+++ OOP/ödevler/vize_ödevi_1/main.java
@@ -0,0 +1,47 @@
+
+package javaHomework;
+
+
+public class Main {
+
+ 
+    public static void main(String[] args) {
+        
+        float trianglePerimeter = MyUtils.trianglePerimeter(3, 3, 3);
+        System.out.print("The perimeter of the triangle is: " + trianglePerimeter + "\n");
+        
+        float triangleArea = MyUtils.triangleArea(3, 3, 3);
+        System.out.print("The area of the triangle is: " + triangleArea + "\n");
+        
+         float polygonPerimeter = MyUtils.polygonPerimeter(5, 10, 4, 8);
+        System.out.print("The perimeter of the plygon is: " + polygonPerimeter + "\n");
+        
+         float polygonArea = MyUtils.polygonArea(10, 6);
+        System.out.print("The area of the plygon is: " + polygonArea + "\n");
+        
+        
+        
+        
+        // -100 ile 100 arasında tutan dizi
+        int [] array = new int[201];
+        int temp = -100; 
+        for(int i = 0; i < array.length; i++)
+        {
+            array[i] = temp;
+            temp ++;
+        }
+        
+        double [] results = MyUtils.functionsResult(array);
+        
+        for(int i = 0; i < results.length; i++)
+        {
+            System.out.print(results[i] + "\n");
+            
+        }
+        
+        
+        
+    }
+      
+    
+}
